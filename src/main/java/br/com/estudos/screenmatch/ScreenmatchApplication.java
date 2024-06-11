ec82bf44cddea79ab58c5ec91f9b8b78ec673f0a
Marcus Lucena <marcus-lucena@outlook.com>
2024-06-11 05:51:27 -0300

    first commit

---
+++ src/main/java/br/com/estudos/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,26 @@
+package br.com.estudos.screenmatch;
+
+import br.com.estudos.screenmatch.model.DadosSerie;
+import br.com.estudos.screenmatch.service.ConsumoApi;
+import br.com.estudos.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://omdbapi.com/?t=gilmore+girls&apikey=118c8665");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
